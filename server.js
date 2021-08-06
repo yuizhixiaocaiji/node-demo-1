024b0fb04e5c9ad5f7741ebcd8df1e6fc2bdd01e
yuizhixiaocaiji <84554833+yuizhixiaocaiji@users.noreply.github.com>
2021-08-06 15:40:30 +0800

    Update server.js

--- server.js
+++ server.js
@@ -24,8 +24,15 @@ var server = http.createServer(function(request, response){
   if(path === '/'){
     response.statusCode = 200
     response.setHeader('Content-Type', 'text/html;charset=utf-8')
-    response.write(`<!DOCTYPE html>
-    <html><head></head><body><h1>我是一个标题标签</h1></body></html>`)
+    response.write(`
+    <!DOCTYPE html>
+    <head>
+      <link rel="stylesheet" href="/x">
+    </head>
+    <body>
+      <h1>我是一个标题标签</h1>
+    </body>
+    `)
     response.end()
   } else if(path === '/x'){
     response.statusCode = 200
